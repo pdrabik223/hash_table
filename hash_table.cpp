eb1a1adb52802c29df1721172a83b0564662c846
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-03-22 13:57:23 +0100

    added iterator, at this point in time it works, but i need to rethink if the implementation is correct

--- hash_table.cpp
+++ hash_table.cpp
@@ -10,18 +10,20 @@ unsigned read_file(const char *file_path);
 unsigned read_console();
 
 
-
-
-
 int main() {
     std::cout << "Hello CMake." << std::endl;
     h_table<int, char> tab(10);
     tab.push(my::pair<int, char>(0, 'k'));
-    std::cout << "val : " << tab[0] << std::endl;
-
     tab.push(my::pair<int, char>(10, 's'));
     tab.push(my::pair<int, char>(20, 'l'));
+
+
+    std::cout << "val : " << tab[0] << std::endl;
     std::cout << "val2 : " << tab[20] << std::endl;
+    for (auto i:tab) {
+        std::cout << i.first<<"\t"<<i.second<<std::endl;
+    }
+
 
     system("pause");
     return 0;
--- hash_table.h
+++ hash_table.h
@@ -4,11 +4,45 @@
 #pragma once
 
 #include <iostream>
+
 #include "pair.h"
 #include "hash_function.h"
+template<typename h_table>
+class iterator
+{
+
+public:
+    using ValueType = typename h_table::ValueType;
+
+    ValueType* current_ptr;
+
+public:
+    iterator(ValueType*  x) :current_ptr(x) {}
+    iterator(const iterator& mit) : current_ptr(mit.current_ptr) {}
+    iterator& operator++() {++current_ptr;return *this;}
+    iterator operator++(int) {iterator tmp(*this); operator++(); return tmp;}
+    bool operator==(const iterator& rhs) const {return current_ptr==rhs.current_ptr;}
+    bool operator!=(const iterator& rhs) const {return current_ptr!=rhs.current_ptr;}
+    ValueType& operator*() {return *current_ptr;}
+};
+
 template<class KEY, class VAL>
 class h_table {
 public:
+
+
+
+    using ValueType =  my::pair<KEY, VAL>;
+
+    iterator<h_table<KEY,VAL>> begin() {
+        return iterator<h_table<KEY,VAL>>(storage_);
+    };
+
+    iterator<h_table<KEY,VAL>> end() {
+        return iterator<h_table<KEY,VAL>>(&storage_[size_]);
+    };
+
+
 
     h_table(size_t size) {
         operations_ = new open_addressing<KEY, VAL>();
